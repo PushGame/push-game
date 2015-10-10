8dceb106e3137c551dfa41e580ad4412126d4115
Alex Hisa <alexhisa@calvisitor-10-105-137-76.CalVisitor.1918.Berkeley.EDU>
2015-10-10 02:57:02 -0700

    added real sprites

--- static/game.js
+++ static/game.js
@@ -1,12 +1,41 @@
 ﻿var socket;
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
 
 function preload() {
-    game.load.image('star', 'static/assets/star.png');
-}
+    game.load.spritesheet('dude', 'static/assets/games/starstruck/dude.png', 32, 48);
+    game.load.image('background', 'static/assets/games/starstruck/background2.png');}
+
+
+var player;
+var facing = 'left';
+var jumpTimer = 0;
+var cursors;
+var jumpButton;
+var bg;
 
 function create() {
     socket = io();
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+    game.physics.arcade.gravity.y = 300;
+
+    player = game.add.sprite(32, 320, 'dude');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 1000;
+    player.body.maxVelocity.y = 500;
+    player.body.setSize(20, 32, 5, 16);
+
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('turn', [4], 20, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
     socket.on('new user', function () {
         drawNewCircle();
@@ -14,8 +43,65 @@ function create() {
 }
 
 function update() {
+
+    // game.physics.arcade.collide(player, layer);
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -150;
+
+        if (facing != 'left')
+        {
+            player.animations.play('left');
+            facing = 'left';
+        }
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 150;
+
+        if (facing != 'right')
+        {
+            player.animations.play('right');
+            facing = 'right';
+        }
+    }
+    else
+    {
+        if (facing != 'idle')
+        {
+            player.animations.stop();
+
+            if (facing == 'left')
+            {
+                player.frame = 0;
+            }
+            else
+            {
+                player.frame = 5;
+            }
+
+            facing = 'idle';
+        }
+    }
+    
+    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    {
+        player.body.velocity.y = -500;
+        jumpTimer = game.time.now + 750;
+    }
+
 }
 
+function render () {
+
+    // game.debug.text(game.time.physicsElapsed, 32, 32);
+    // game.debug.body(player);
+    game.debug.bodyInfo(player, 16, 24);
+
+}
 function drawNewCircle() {
-    game.add.sprite(game.world.width * Math.random(), game.world.height * Math.random(), 'star');
+	game.add.sprite(32, 320, 'dude');
 }
