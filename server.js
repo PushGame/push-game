7394c9a1083c643e811c252a250b0135c604ef4f
Qwaz <qwazpia@gmail.com>
2015-10-10 03:52:51 -0700

    Implement first prototype

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var uuid = require('uuid');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/game/game.html');
@@ -9,6 +10,9 @@ app.get('/', function (req, res) {
 // static serve everything under game directory
 app.use('/static', express.static(__dirname + '/static'));
 
+var sockets = [];
+var userList = [];
+
 var io = require('socket.io')(http);
 
 http.listen(3000, function () {
@@ -16,8 +20,48 @@ http.listen(3000, function () {
 });
 
 io.on('connection', function (socket) {
-    console.log('new user connected');
-    io.emit('new user');
+    // Create user information
+    var user = {
+        id: uuid.v1(),
+        x: 800 * Math.random(),
+        y: 600 * Math.random()
+    };
+    
+    // Send connection information
+    socket.emit('connect', {
+        id: user.id,
+        x: user.x,
+        y: user.y,
+        userList: userList
+    });
+
+    socket.broadcast.emit('new user', {
+        id: user.id,
+        x: user.x,
+        y: user.y
+    });
+    
+    socket.user = user;
+    userList.push(user);
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('logout', socket.user.id);
+
+        var i;
+        for (i = 0; i < userList.length; i++) {
+            if (userList[i].id === socket.user.id) {
+                userList[i] = userList[userList.length - 1];
+                userList.pop();
+            }
+        }
+    });
+
+    socket.on('move', function (data) {
+        socket.user.x = data.x;
+        socket.user.y = data.y;
+
+        socket.broadcast.emit('move', socket.user);
+    });
 });
 
 /* 
