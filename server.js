2a95e6ad7e8823449d000439878dd9925def097c
Qwaz <qwazpia@gmail.com>
2015-10-10 01:17:56 -0700

    Add some more chat stuff

--- server.js
+++ server.js
@@ -2,7 +2,19 @@
 var http = require('http').Server(app);
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('a user disconnected');
+    });
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function () {
