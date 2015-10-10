deeb02787e1f8ef67af80faf2521c66716f88f8c
Qwaz <qwazpia@gmail.com>
2015-10-10 01:50:27 -0700

    Add phaser.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-﻿var app = require('express')();
+﻿var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/game/game.html');
 });
+
+// static serve everything under game directory
+app.use('/static', express.static(__dirname + '/static'));
 
 var io = require('socket.io')(http);
 
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+/* 
+ * Used for socket.io chat tutorial
+ * 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 io.on('connection', function (socket) {
     console.log('a user connected');
     socket.on('disconnect', function () {
@@ -16,7 +30,4 @@ io.on('connection', function (socket) {
         io.emit('chat message', msg);
     });
 });
-
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
+*/
